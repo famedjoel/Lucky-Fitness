6f2e7821534b9f570309ede15b10c198232512be
famedjoel <up2090849@myport.ac.uk>
2024-02-09 22:28:52 +0000

    change dmy settings to more of a page not a dropdown

--- Client/setting.js
+++ Client/setting.js
@@ -1,18 +1,18 @@
-function init() {
-    document.querySelector('#settings').addEventListener('click', toggleSettings);
-}
-function toggleSettings() {
-    const settingsContent = document.querySelector('#settings-content');
-    settingsContent.style.display = (settingsContent.style.display === "block") ? "none" : "block";
-}
+// function init() {
+//     document.querySelector('#settings').addEventListener('click', toggleSettings);
+// }
+// function toggleSettings() {
+//     const settingsContent = document.querySelector('#settings-content');
+//     settingsContent.style.display = (settingsContent.style.display === "block") ? "none" : "block";
+// }
 
-init()
+// init()
 
-const settingsDropdown = document.querySelector('#settings-dropdown');
-      
-settingsDropdown.addEventListener('change', function() {
-  const selectedOption = this.value;
-  if (selectedOption !== "") {
-    window.location.href = selectedOption;
-  }
+
+const settingsIcon = document.querySelector('#settings-icon');
+
+// Add a click event listener to the settings icon
+settingsIcon.addEventListener('click', function () {
+  // Navigate to the settings page
+  window.location.href = 'settings.html';
 });
